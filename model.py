447d3bed4a9e85fab255e36f6c322ad681d64333
unknown <steven.e.ingram@gmail.com>
2020-05-23 22:04:17 -0700

    Initial commit.

---
+++ model.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from functools import reduce
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed, fc1_units=128, fc2_units=64):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+        """
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        "*** YOUR CODE HERE ***"
+        
+        self.fc = nn.Sequential(
+            nn.Linear(state_size, fc1_units),
+            nn.ReLU(),
+            nn.Linear(fc1_units, fc2_units),
+            nn.ReLU(),
+            nn.Linear(fc2_units, action_size))
+        
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        return self.fc(state)
